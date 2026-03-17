1f08695e055d3beccf222844293eee0d6acc315e
G3RGES <georgios.nashaat@gmail.com>
2026-03-17 07:18:19 +0200

    Update page.js

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <h1>Cabins</h1>;
+import Counter from "../components/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://dummyjson.com/users");
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins</h1>
+      <ul>
+        {data.users.map((user) => (
+          <li key={user.id}>{user.firstName}</li>
+        ))}
+      </ul>
+
+      <Counter users={data} />
+    </div>
+  );
 }

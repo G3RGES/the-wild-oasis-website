604e0872db75f9de32f82afb5582b69162cee841
G3RGES <georgios.nashaat@gmail.com>
2026-03-24 10:16:31 +0200

    Update Filter.js

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,12 +1,44 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter);
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }
+
   return (
     <div className="border border-primary-800 flex ">
-      <button className="px-5 py-2 hover:bg-primary-700">All cabins</button>
-      <button className="px-5 py-2 hover:bg-primary-700">Small cabins</button>
-      <button className="px-5 py-2 hover:bg-primary-700">Medium cabins</button>
-      <button className="px-5 py-2 hover:bg-primary-700">Large cabins</button>
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("all")}
+      >
+        All cabins
+      </button>
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("small")}
+      >
+        Small cabins
+      </button>
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("medium")}
+      >
+        Medium cabins
+      </button>
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("large")}
+      >
+        Large cabins
+      </button>
     </div>
   );
 }

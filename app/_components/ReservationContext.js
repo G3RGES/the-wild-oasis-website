9cfe5fe395bf97f84d83b2e9035079405735fd5e
G3RGES <georgios.nashaat@gmail.com>
2026-03-25 12:57:07 +0200

    Update ReservationContext.js

--- app/_components/ReservationContext.js
+++ app/_components/ReservationContext.js
@@ -12,8 +12,10 @@ const initialState = {
 function ReservationProvider({ children }) {
   const [range, setRange] = useState(initialState);
 
+  const resetRange = () => setRange(initialState);
+
   return (
-    <ReservationContext.Provider value={{ range, setRange }}>
+    <ReservationContext.Provider value={{ range, setRange, resetRange }}>
       {children}
     </ReservationContext.Provider>
   );

342ec8c46d5e7098a8e4cdbdc4e57b684b0ff112
G3RGES <georgios.nashaat@gmail.com>
2026-03-30 20:31:26 +0200

    Update DeleteReservation.js

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -3,6 +3,7 @@
 import { TrashIcon } from "@heroicons/react/24/solid";
 import { deleteReservation } from "../_lib/actions";
 import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
   const [isPending, startTransition] = useTransition();
@@ -19,8 +20,16 @@ function DeleteReservation({ bookingId }) {
       disabled={isPending}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      {isPending ? (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      ) : (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      )}
     </button>
   );
 }

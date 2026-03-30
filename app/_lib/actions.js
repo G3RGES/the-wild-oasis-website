1d362c623b2c57d0c69c27424cef816973b42f6d
G3RGES <georgios.nashaat@gmail.com>
2026-03-30 20:16:41 +0200

    Update actions.js

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,7 +3,8 @@
 import { revalidatePath } from "next/cache";
 
 import { auth, signIn, signOut } from "./auth";
-import { updateGuest } from "./data-service";
+import { getBookings, updateGuest } from "./data-service";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -38,4 +39,24 @@ export async function updateProfile(formData) {
   await updateGuest(session.user.guestId, updateData);
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("Not authenticated");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("Booking does not belong to the authenticated user");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("/account/reservations");
 }

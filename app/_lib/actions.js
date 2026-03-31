c492a781e93bbe93e0ccf1dec8d1d4769d736aa9
G3RGES <georgios.nashaat@gmail.com>
2026-03-31 14:17:15 +0200

    Update actions.js

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -5,6 +5,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { getBookings, updateGuest } from "./data-service";
 import { supabase } from "./supabase";
+import { redirect } from "next/navigation";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -59,4 +60,44 @@ export async function deleteReservation(bookingId) {
   if (error) throw new Error("Booking could not be deleted");
 
   revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  // console.log(formData);
+
+  //* 1. AUTHENTICATION & AUTHORIZATION CHECKS
+  const bookingId = Number(formData.get("bookingId"));
+
+  const session = await auth();
+  if (!session) throw new Error("Not authenticated");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("Booking does not belong to the authenticated user");
+
+  //* 2. INPUT VALIDATION & SANITIZATION
+
+  const updatedFields = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000), // Limit observations to 1000 characters
+  };
+
+  // console.log(updatedFields);
+
+  //* 3. UPDATE THE BOOKING
+  const { error } = await supabase
+    .from("bookings")
+    .update(updatedFields)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  if (error) throw new Error("Booking could not be updated");
+
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  redirect("/account/reservations");
 }
